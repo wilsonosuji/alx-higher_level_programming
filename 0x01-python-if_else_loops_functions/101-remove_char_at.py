e9a2b3e9c541125279de2a415b748042cc64c838
wilsonosuji <wilsonosuji88@gmail.com>
2022-11-29 05:20:13 -0800

    If Else Loops Functions

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Author - Wilson Osuji
+
+def remove_char_at(str, n):
+        if n < 0:
+ #!/usr/bin/python3
+ # Author - Wilson Osuji
+
+ def remove_char_at(str, n):
+         if n < 0:
+                     return (str)
+                     return (str[:n] + str[n+1:])#!/usr/bin/python3
+                 # Author - Wilson Osuji
+
+                 def remove_char_at(str, n):
+                         if n < 0:
+              
